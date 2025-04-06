3498506ad50606487346d96cbf17e733dc57793c
MaNnu81 <mannuspamm@gmail.com>
2025-04-07 00:55:54 +0200

    manca css dettaglio

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,13 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { PokemonService } from '../../services/pokemon.service';
-import { PokemonCardComponent } from '../pokemon-card/pokemon-card.component';
+
 import { type PokemonModel } from '../../model/pokemon-model';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
-  imports: [PokemonCardComponent, CommonModule, RouterModule],
+  imports: [ CommonModule, RouterModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
